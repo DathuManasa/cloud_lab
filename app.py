b81ad9ce453d3c5c67326dc566e650e393cad77c
DathuManasa <dathumanasa27@gmail.com>
2025-07-31 14:32:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def hello():
     return "hello from flask!"
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # default to 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port)
